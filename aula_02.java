57143e0702156975a476aa76443ee530f4ac2610
hallisonx <38287389+hallisonx@users.noreply.github.com>
2019-06-04 10:56:06 -0300

    Update aula_02.java

--- aula_02.java
+++ aula_02.java
@@ -23,10 +23,22 @@ public class Main
 
 
 		int valor = 0;
-
 		while(valor < 5){
 		    System.out.printf("valor é: %s \n", valor);
 		    valor++;
+		}
+		
+		
+		for(int i=3; i<10; i+=2) {
+		    System.out.println(" i : " + i);
+		}
+		
+		int v = 3;
+		while (v <= 12) {
+		    if(v % 3 == 1) {
+		        System.out.println(v + " mod 3 = " + v%3);
+		    }
+		    v++;
 		}
 
 	}
